6ea7d2d63ae2816393754099b71e5da78a6ea2f3
dylannvms <dylanvms@gmail.com>
2026-09-23 19:18:32 -0700

    Estructura inicial del proyecto Loteria

---
+++ src/main/java/com/mycompany/loteria_equipo3/Loteria_Equipo3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loteria_equipo3;
+
+/**
+ *
+ * @author Dylan
+ */
+public class Loteria_Equipo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
