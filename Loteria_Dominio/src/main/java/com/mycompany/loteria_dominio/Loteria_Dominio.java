788670f3989ae2e1ca8210b83745a3e651d0d5a6
Dnv14 <diego.navarro.140706@gmail.com>
2026-09-26 01:36:17 -0700

    creacion del proyecto dominio

---
+++ Loteria_Dominio/src/main/java/com/mycompany/loteria_dominio/Loteria_Dominio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loteria_dominio;
+
+/**
+ *
+ * @author Diego
+ */
+public class Loteria_Dominio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
